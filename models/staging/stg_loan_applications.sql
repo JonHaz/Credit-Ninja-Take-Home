7095794666617de4ee0ed13ac0081f7fdc8a7ca9
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-05 21:46:38 -0400

    Create stg_loan_applications.sql

---
+++ models/staging/stg_loan_applications.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+SELECT
+    application_id,
+    customer_id,
+    CAST(application_date AS DATE) AS application_date,
+    CAST(loan_amount_requested AS DECIMAL(10,2)) AS loan_amount_requested,
+    status,
+    updated_at
+FROM {{ source('bronze', 'loan_applications') }}
