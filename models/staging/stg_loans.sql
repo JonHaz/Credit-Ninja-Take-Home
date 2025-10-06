58d9687120859749681ce61dc04ffeffd3044a7c
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-05 23:09:32 -0400

    Update stg_loans.sql

--- models/staging/stg_loans.sql
+++ models/staging/stg_loans.sql
@@ -12,8 +12,8 @@ SELECT
     customer_id,
     CAST(loan_amount AS DECIMAL(10,2)) AS loan_amount,
     CAST(interest_rate AS DECIMAL(5,2)) AS interest_rate,
-    start_date,
-    end_date,
+    CAST(start_date AS DATE) AS start_date,
+    CAST(end_date AS DATE) AS end_date,
     status,
     CURRENT_TIMESTAMP() AS record_loaded_at
 FROM {{ source('snowflake', 'loans') }}
