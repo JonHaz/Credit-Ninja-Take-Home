1d9602cd40a781f29869e871b3f16b471976133b
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-05 21:47:30 -0400

    Create stg_payments.sql

---
+++ models/staging/stg_payments.sql
@@ -0,0 +1,10 @@
+{{ config(materialized='table') }}
+
+SELECT
+    payment_id,
+    loan_id,
+    customer_id,
+    CAST(payment_amount AS DECIMAL(10,2)) AS payment_amount,
+    payment_date,
+    payment_type
+FROM {{ source('bronze', 'payments') }}
