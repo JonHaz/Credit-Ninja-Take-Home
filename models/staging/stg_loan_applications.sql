3cbec2392322060d3aba098fe024fddf0fe877b8
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-05 23:04:46 -0400

    Update stg_loan_applications.sql

--- models/staging/stg_loan_applications.sql
+++ models/staging/stg_loan_applications.sql
@@ -1,4 +1,10 @@
-{{ config(materialized='table') }}
+{{
+    config(
+        materialized='incremental',
+        unique_key='loan_id',
+        incremental_strategy='merge'
+    )
+}}
 
 SELECT
     application_id,
@@ -6,5 +12,6 @@ SELECT
     CAST(application_date AS DATE) AS application_date,
     CAST(loan_amount_requested AS DECIMAL(10,2)) AS loan_amount_requested,
     status,
-    updated_at
+    updated_at,
+    CURRENT_TIMESTAMP() AS record_loaded_at
 FROM {{ source('snowflake', 'loan_applications') }}
