39ca24860bcf15a8eb79e898b82349071fbcec4c
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-05 21:46:04 -0400

    Update stg_customers.sql

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -2,8 +2,8 @@
 
 SELECT
     customer_id,
-    first_name,
-    last_name,
-    email,
+    TRIM(first_name) AS first_name,
+    TRIM(last_name) AS last_name,
+    LOWER(email) AS email,
     created_at
 FROM {{ source('bronze', 'customers') }}
