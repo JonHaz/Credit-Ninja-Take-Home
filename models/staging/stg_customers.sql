896a0912a7d3053622a563295ecd38dd52e07b97
Jonathan Hazeley <jonathanhazeley@gmail.com>
2025-10-06 10:44:00 -0400

    Update stg_customers.sql
    fixed issue that referenced source incorrectly

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -6,13 +6,12 @@
     )
 }}
 
-
-    SELECT
-        customer_id,
-        TRIM(first_name) AS first_name,
-        TRIM(last_name) AS last_name,
-        LOWER(TRIM(email)) AS email,
-        created_at AS customer_created_at,
-        CURRENT_TIMESTAMP() AS record_loaded_at
-    FROM {{ ref('stg_customers') }}
+SELECT
+    customer_id,
+    TRIM(first_name) AS first_name,
+    TRIM(last_name) AS last_name,
+    LOWER(email) AS email,
+    created_at,
+    CURRENT_TIMESTAMP() AS record_loaded_at
+FROM {{ source('snowflake', 'customers') }}
 
